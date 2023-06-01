2594a1b8b2b44665edb38910384acebab58b709c
michellec20 <cc18064@ues.edu.sv>
2023-05-31 22:39:22 -0600

    Creación models y migraciones

--- HDP115/efectoEconomia/models.py
+++ HDP115/efectoEconomia/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+tipo_categoria =[
+    (1, 'Economía'),
+    (2, 'Cambio climático')
+]
+    
 # Create your models here.
+class noticia(models.Model):
+    idnoticia = models.AutoField(primary_key=True)
+    fecha = models.DateField()
+    titulo = models.CharField(max_length=200, default="", blank=True)
+    contenido = models.TextField(max_length=500, default="", blank=True)
+    categoria = models.IntegerField(
+        blank=True,
+        choices=tipo_categoria
+    )
