63b2c6406dccb032603505b28f8a2e5df143f9a2
michellec20 <cc18064@ues.edu.sv>
2023-06-01 20:39:11 -0600

    View index

--- HDP115/efectoEconomia/views.py
+++ HDP115/efectoEconomia/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, TemplateView, ListView
+from .mixins import *
 
 # Create your views here.
+class index(GroupRequiredMixin, TemplateView):
+    group_required = [u'administrador']
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        context=super().get_context_data(**kwargs)
+        return super().dispatch(request, *args, **kwargs)
+    template_name = 'index.html'
