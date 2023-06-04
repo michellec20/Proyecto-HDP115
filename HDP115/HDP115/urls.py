94639e11b8e243bac376489d1292709c19c8c0d2
MariaLuisaGav <marialuisagavarrete20@gmail.com>
2023-06-03 20:01:35 -0600

    noticia

--- HDP115/HDP115/urls.py
+++ HDP115/HDP115/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('',index.as_view(), name='home'),
     path('accounts/login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout')
+    path('accounts/gestionarNoticia/', LoginView.as_view(template_name='gestionarNoticia.html'), name='gestionarNoticia'),
 ]
