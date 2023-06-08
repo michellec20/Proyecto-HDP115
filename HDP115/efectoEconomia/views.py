8d4244a37ec651503886a3cfd2ce1864e06d1af5
michellec20 <cc18064@ues.edu.sv>
2023-06-08 00:03:03 -0600

    templates y views.py

--- HDP115/efectoEconomia/views.py
+++ HDP115/efectoEconomia/views.py
@@ -1,42 +1,46 @@
-from urllib import request
 from django.shortcuts import render, redirect
-from .forms import *
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib import messages
+from django.urls import reverse_lazy
+from .models import noticia
+from .forms import NoticiaForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .mixins import *
 from django.views.generic import CreateView, TemplateView, ListView
 from django.http import HttpResponse, HttpResponseRedirect
-from .mixins import *
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .models import noticia
-from django.contrib import messages
+
 
 # Create your views here.
 class index(GroupRequiredMixin, TemplateView):
     group_required = [u'administrador']
+    
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         context=super().get_context_data(**kwargs)
         return super().dispatch(request, *args, **kwargs)
+    
     template_name = 'index.html'
 
 class agregarNoticia(GroupRequiredMixin, CreateView):
     group_required = [u'administrador']
+    
     @method_decorator(login_required)
-
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
+    def dispatch(self, *args, **kwargs):
+        return super(agregarNoticia, self).dispatch(*args, **kwargs)
 
     template_name = 'agregarNoticia.html'
     model = noticia
     form_class = NoticiaForm
 
     def get_url_redirect(self, **kwargs):
-        context=super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         return reverse_lazy('home')
     
     def form_valid(self, form, **kwargs):
-        context=super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         noticia = form.save(commit=False)
         
         try:
@@ -45,68 +49,79 @@ class agregarNoticia(GroupRequiredMixin, CreateView):
         except Exception:
             noticia.delete()
             messages.success(self.request, 'Ocurrió un error al agregar la noticia')
+
         return HttpResponseRedirect(self.get_url_redirect())
     
-    class gestionarNoticia(GroupRequiredMixin, TemplateView):
-        group_required = [u'administrador']
+class gestionarNoticia(GroupRequiredMixin, View):
+    group_required = [u'administrador']
+    
     @method_decorator(login_required)
-
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
     template_name = 'gestionarNoticia.html'
     model = noticia
+    form_class = NoticiaForm
 
-    def get_success_url(self):
+    def get(self, request):
+        noticia_obj = noticia.objects.all()  # Obtén todas las noticias del modelo
+        context = {'noticias': noticia_obj}
+        return render(request, self.template_name, context)
+    
+    """def get_success_url(self):
         return reverse_lazy('home')
     
     def form_valid(self, form):
         form.instance.usuario = self.request.user
         return super().form_valid(form)
         
-    def home(request):
+    def get(self, request):
         noticiasListados = noticia.objects.all()
         messages.success(request, '¡Noticia Listada!')
         return render(request, "gestionarNoticia.html", {"noticias": noticiasListados})
     
-    def registrarNoticia(request):
-        id=request.POST['txtId']
-        fecha=request.POST['txtFecha']
-        autor=request.POST['txtAutor']
-        categoria=request.POST['txtCategoria']
-        titulo=request.POST['txtTitulo']
+    def post(self, request):
+        id = request.POST['txtId']
+        fecha = request.POST['txtFecha']
+        autor = request.POST['txtAutor']
+        categoria = request.POST['txtCategoria']
+        titulo = request.POST['txtTitulo']
 
-        noticia = NoticiaForm.objects.create(
-        id=id, fecha=fecha, autor=autor, categoria=categoria, titulo=titulo)
+        noticia = noticia.objects.create(
+            id=id, fecha=fecha, autor=autor, categoria=categoria, titulo=titulo)
         messages.success(request, '¡Noticia Registrada!')
         return redirect('/')
     
-    def edicionNoticia(request, id):
-        noticia = noticia.objects.get(id=id)
-        return render(request, "edicionNoticia.html", {"noticia": noticia})
+    def edicionNoticia(self, request, id):
+        noticia_obj = noticia.objects.get(id=id)
+        return render(request, "edicionNoticia.html", {"noticia": noticia_obj})
     
-    def editarNoticia(request):
+    def editarNoticia(self, request):
         id=request.POST['txtId']
         fecha=request.POST['txtFecha']
         autor=request.POST['txtAutor']
         categoria=request.POST['txtCategoria']
         titulo=request.POST['txtTitulo']
 
-        noticia = noticia.objects.get(id=id)
-        noticia.fecha = fecha
-        noticia.autor = autor
-        noticia.categoria = categoria
-        noticia.titulo = titulo
-        noticia.save()
+        noticia_obj = noticia.objects.get(id=id)
+        noticia_obj.fecha = fecha
+        noticia_obj.autor = autor
+        noticia_obj.categoria = categoria
+        noticia_obj.titulo = titulo
+        noticia_obj.save()
 
         messages.success(request, '¡Noticia Actualizada!')
 
         return redirect('/')
     
     def eliminarNoticia(request, id):
-        noticia = noticia.objects.get(id=id)
-        noticia.delete()
+        noticia_obj = noticia.objects.get(id=id)
+        noticia_obj.delete()
 
         messages.success(request, '¡Noticia Eliminada!')
 
-        return redirect('/')
+        return redirect('/')"""
+    
+    
+    
+        
