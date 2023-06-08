421c8612b2b2b0965dc5e0be18760c2011783047
michellec20 <cc18064@ues.edu.sv>
2023-06-08 00:00:11 -0600

    Modificación settings

--- HDP115/HDP115/settings.py
+++ HDP115/HDP115/settings.py
@@ -129,8 +129,8 @@ STATICFILES_DIRS = [
     'static'
 ]
 
-LOGIN_REDIRECT_URL = 'home/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
